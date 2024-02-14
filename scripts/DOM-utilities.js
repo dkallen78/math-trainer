f7f9c5ca4a07e9876683e7f15c579d37cf790e09
dkallen78 <dkallen78@gmail.com>
2024-02-14 13:07:46 -0600

    Added make, .header, and .main to DOM-utilities.js

--- scripts/DOM-utilities.js
+++ scripts/DOM-utilities.js
@@ -7,3 +7,34 @@ function clear(...elements) {
 
   elements.forEach(x => x.innerHTML = "");
 }
+
+function make(type, id, classes) {
+  //----------------------------------------------------//
+  //Returns an DOM element                              //
+  //----------------------------------------------------//
+  //type(string): type of DOM element to create         //
+  //id(string): id of the element                       //
+  //classes(string): classes to add to the element      //
+  //----------------------------------------------------//
+  //return(element): DOM element                        //
+  //----------------------------------------------------//
+
+  let element = document.createElement(type);
+  if (typeof id === "string") {element.id = id}
+  if (typeof classes === "string") {
+    element.classList.add(classes);
+  } else if (typeof classes === "object") {
+    classes.forEach(x => element.classList.add(x));
+  }
+  return element;
+}
+
+make.header = function(id, classes) {
+  let header = make("header", id, classes);
+  return header;
+}
+
+make.main = function(id, classes) {
+  let main = make("main", id, classes);
+  return main;
+}
---
+++ scripts/title-menu.js
@@ -0,0 +1,11 @@
+function makeTitleScreen() {
+  //----------------------------------------------------//
+	//Makes the title screen                              //
+	//----------------------------------------------------//
+
+  clear(document.body);
+
+  let titleScreen = make.main("title-screen", "screen-flex");
+
+  document.body.appendChild(titleScreen);
+}
