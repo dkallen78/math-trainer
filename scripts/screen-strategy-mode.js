3d193bfb815db123926863b5f0f460ea7c42eb19
dkallen78 <dkallen78@gmail.com>
2024-02-15 11:01:02 -0600

    finished building out the strategy mode menus

--- scripts/screen-strategy-mode.js
+++ scripts/screen-strategy-mode.js
@@ -108,7 +108,7 @@ async function makeStrategyGroupsScreen(operation) {
           remove.class(groupButton, "button-inactive");
           set.click(groupButton, async () => {
             playTone(randomNote());
-            //await makeSkillsScreen(skills[operation][i]);
+            await makeStrategyDetailScreen(operation[group]);
             resolve(false);
           });
         }
@@ -128,7 +128,7 @@ async function makeStrategyGroupsScreen(operation) {
     const strategyGroupsScreen = make.main("strategy-groups-screen", ["screen", "flex-column"]);
 
       for (const group in operation) {
-        const groupButton = make.button(operation[group][0], `strategy-groups-screen__${operation[group][0]}-button`, ["button-medium", "button-incactive"]);
+        const groupButton = make.button(operation[group][0], `strategy-groups-screen__${operation[group][0]}-button`, ["button-medium", "button-inactive"]);
         strategyGroupsScreen.appendChild(groupButton);
       }
 
@@ -137,6 +137,89 @@ async function makeStrategyGroupsScreen(operation) {
       
     await fadeTransition(strategyGroupsScreen);
       
+    await waitForButton()
+      .then((exit) => {quit = exit});
+  }
+}
+
+async function makeStrategyDetailScreen(group) {
+  //----------------------------------------------------//
+  //Makes the screen where the user selects a strategy  //
+  //  to practice. This is the screen right before the  //
+  //  primary math loop                                 //
+  //----------------------------------------------------//
+  //skill(array[objects]): the array of skills to be    //
+  //  displayed                                         //
+  //----------------------------------------------------//
+
+  async function waitForButton() {
+
+    return new Promise((resolve, reject) => {
+      const strategyDetail = get("strategy-detail-screen__strategy-detail");
+
+      for (let i = 1; i < group.length; i++) {
+
+        if (group[i].test()) {
+          const strategyButton = get(`strategy-detail-screen__strategy-grid__button-${i}`);
+          const id = group[i].id;
+          const skillPassed = user[id[0]][id[1]][id[2]];
+
+          remove.class(strategyButton, "button-inactive");
+
+          if (typeof skillPassed === "undefined") {
+            set.class(strategyButton, "button-ready");
+          }
+
+          set.click(strategyButton, async () => {
+            playTone(randomNote());
+            strategyDetail.innerHTML = group[i].name;
+
+            const startButton = get("strategy-detail-screen__start-button");
+            remove.class(startButton, "button-inactive");
+            set.click(startButton, async () => {
+              playTone(randomNote());
+
+              resolve(false);
+            })
+          })
+        }
+      }
+
+      const backButton = get("strategy-detail-screen__back-button");
+      set.click(backButton, () => {
+        playTone(randomNote());
+        resolve(true);
+      })
+    })
+  }
+
+  let quit = false;
+  while(!quit) {
+
+    const strategyDetailScreen = make.main("strategy-detail-screen", ["screen", "grid"]);
+
+      const strategyDetailScreenInfo = make.header("strategy-detail-screen__info", "marquee");
+        strategyDetailScreenInfo.innerHTML = group[0];
+      strategyDetailScreen.appendChild(strategyDetailScreenInfo);
+
+      const strategyDetail = make.section("strategy-detail-screen__strategy-detail");
+      strategyDetailScreen.appendChild(strategyDetail);
+
+      const strategyGrid = make.section("strategy-detail-screen__strategy-grid", "grid");
+        for (let i = 1; i < group.length; i++) {
+          const button = make.button(i, `strategy-detail-screen__strategy-grid__button-${i}`, "button-inactive");
+          strategyGrid.appendChild(button);
+        }
+      strategyDetailScreen.appendChild(strategyGrid);
+
+      const startButton = make.button("Start", "strategy-detail-screen__start-button", ["button-big", "button-inactive"]);
+      strategyDetailScreen.appendChild(startButton);
+
+      const backButton = make.button("Back", "strategy-detail-screen__back-button", "button-big");
+      strategyDetailScreen.appendChild(backButton);
+
+    await fadeTransition(strategyDetailScreen);
+
     await waitForButton()
       .then((exit) => {quit = exit});
   }
