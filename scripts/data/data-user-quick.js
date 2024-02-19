61b578727eee01d64c2edbbca4f0c70831685c0b
dkallen78 <dkallen78@gmail.com>
2024-02-19 12:56:06 -0600

    adding a quick traverse version

---
+++ scripts/data/data-user-quick.js
@@ -0,0 +1,34 @@
+let user = {
+  soundOn: true,
+  activeScale: scales.major,
+  keyNote: 0,
+  keyOctave: 4,
+  get activeKey() {
+    return ((this.keyOctave - 2) * 12) + this.keyNote;
+  },
+  addition: {
+    fundamentals: [],
+    placeValue: [],
+    reorder: [],
+    partition: [],
+    compensation: [],
+    sequence: [],
+    fractions: []
+  },
+  subtraction: {
+    fundamentals: [],
+    partition: [],
+    decomposition: [],
+    sequence: []
+  },
+  multiplication: {
+    fundamentals: [],
+    distribution: [],
+    association: []
+  },
+  division: {
+    fundamentals: []
+  },
+  qDepth: 1,
+  maxAvg: 5000
+}
